9b7ec500e1a5ece29c18aa05bcef8bb00e6c5dde
Gabriel Rosa <gabokuck1990@gmail.com>
2026-09-23 13:35:11 -0600

    step 7

---
+++ app/crear-publicacion/page.tsx
@@ -0,0 +1,9 @@
+import { AppShell } from "../_components/app-shell";
+
+export default function CrearPublicacionPage() {
+  return (
+    <AppShell>
+      <div className="p-8 font-medium">Pantalla pendiente</div>
+    </AppShell>
+  );
+}
---
+++ app/login/page.tsx
@@ -0,0 +1,9 @@
+import { AppShell } from "../_components/app-shell";
+
+export default function LoginPage() {
+  return (
+    <AppShell>
+      <div className="p-8 font-medium">Pantalla pendiente</div>
+    </AppShell>
+  );
+}
